ba08465c1b31cd68f267eb5bdf8b5aa745b82a3d
Madhurya Bharadwaaz <dg.hipl@gmail.com>
2024-02-24 22:39:55 +0530

    added org limit, 5 boards can be created for free plan

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/BoardList.tsx
@@ -1,7 +1,9 @@
 import { FormPopover } from "@/components/form/popover"
 import { Hint } from "@/components/hint"
 import { Skeleton } from "@/components/ui/skeleton"
+import { MAX_FREE_BOARDS } from "@/constants/boards"
 import { db } from "@/lib/db"
+import { getAvailableCount } from "@/lib/org-limit"
 import { auth } from "@clerk/nextjs"
 import { HelpCircle, User2 } from "lucide-react"
 import Link from "next/link"
@@ -19,7 +21,9 @@ export const BoardList = async () => {
         orderBy: {
             createdAt: "desc"
         }
-    })
+    });
+
+    const availableCount = await getAvailableCount();
 
     return (
         <div className="space-y-4">
@@ -51,7 +55,9 @@ export const BoardList = async () => {
                         className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
                         <p className="text-sm">Create new board</p>
 
-                        <span className="text-xs">5 remaining</span>
+                        <span className="text-xs">
+                            {`${MAX_FREE_BOARDS - availableCount} remaining`}
+                        </span>
 
                         <Hint
                             sideOffset={40}
